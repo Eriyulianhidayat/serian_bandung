039a691a1d602235e6687eebd5cd811f986070a8
programer <85097802+Eriyulianhidayat@users.noreply.github.com>
2022-08-08 18:32:57 +0700

    Add files via upload

---
+++ js/waktu.js
@@ -0,0 +1,16 @@
+// $(document).ready(function () {
+// let da= new Date();
+// let time =da.toLocaleString();
+// $('#waktu').html('Sekarang Jam &raquo; '+ time);	
+// });
+
+       
+    function tampilkanwaktu(){
+    // var waktu = new Date();
+    var tgl= new Date();
+    document.getElementById("waktu").innerHTML =tgl;
+    }       
+
+
+
+
